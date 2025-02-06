261265ee9ae28ba524124f40338cba9f16b95ffc
ilaktatoor <cemmnuelaxa@gmail.com>
2025-02-05 23:31:27 -0600

    base

---
+++ flashcardBackend/src/main/java/com/stdevsec/flashcardBackend/FlashcardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stdevsec.flashcardBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardBackendApplication.class, args);
+	}
+
+}
---
+++ flashcardBackend/src/test/java/com/stdevsec/flashcardBackend/FlashcardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdevsec.flashcardBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
