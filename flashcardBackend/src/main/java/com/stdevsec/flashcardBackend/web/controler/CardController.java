95555b23c8386fb909f6f7f1142b395a4227d1ee
ilaktatoor <cemmnuelaxa@gmail.com>
2025-02-05 23:51:50 -0600

    backend

---
+++ flashcardBackend/src/main/java/com/stdevsec/flashcardBackend/web/controler/CardController.java
@@ -0,0 +1,57 @@
+package com.stdevsec.flashcardBackend.web.controler;
+
+import com.stdevsec.flashcardBackend.service.CardService;
+import com.stdevsec.flashcardBackend.web.model.CardModel;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    @Autowired
+    private CardService service;
+
+    @GetMapping
+    public List<CardModel> getAllCards(){
+        return service.getAllCards();
+    }
+
+    @GetMapping("/lenguaje/{lenguajeId}")
+    public List<CardModel> getCardsByLenguaje(@PathVariable Long lenguajeId){
+        return service.getCardsByLenguaje(lenguajeId);
+    }
+
+    @PostMapping
+    public ResponseEntity<CardModel> createCard(@RequestBody CardModel model){
+        CardModel createdCard = service.createCard(model);
+        if(createdCard != null){
+            return ResponseEntity.ok(createdCard);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<CardModel> editCard(@PathVariable Long id, @RequestBody CardModel model){
+        CardModel editedCard = service.createCard(model);
+        if(editedCard != null){
+            return ResponseEntity.ok(editedCard);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long id){
+        if(service.deleteCard(id)){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ flashcardBackend/src/main/java/com/stdevsec/flashcardBackend/web/controler/LenguajeController.java
@@ -0,0 +1,36 @@
+package com.stdevsec.flashcardBackend.web.controler;
+
+import com.stdevsec.flashcardBackend.service.LenguajeService;
+import com.stdevsec.flashcardBackend.web.model.LenguajeModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lenguajes")
+public class LenguajeController {
+
+    @Autowired
+    private LenguajeService service;
+
+    @GetMapping
+    public List<LenguajeModel> getAllLenguajes(){
+        return service.getAllLenguajes();
+    }
+
+    @PostMapping
+    public LenguajeModel createLenguaje(@RequestBody LenguajeModel model){
+        return service.createLenguaje(model);
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> deleteLenguaje(@PathVariable Long id){
+        if (service.deleteLenguaje(id)){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
